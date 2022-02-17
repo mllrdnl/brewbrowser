280fa54a05ee7d883322354b953403fdcca76da8
Mallory Daniel <mllr.dnl@gmail.com>
2022-02-16 19:32:52 -0800

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Container fluid>
+          <Row>
+            <Navbar className="d-flex" bg="dark" variant="dark">
+            
+            <Navbar.Brand href="#home">BrewBrowser</Navbar.Brand>
+            <Nav className="d-flex justify-content-end">
+              <Nav.Link href="#home">Home</Nav.Link>
+              
+              <Nav.Link href="#">Favorites</Nav.Link>
+              <Nav.Link href="#">Wish List</Nav.Link>
+              
+            </Nav>
+        
+          </Navbar>
+      
+      </Row>
+    </Container>
+  </div>
+    
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">BrewBrowser</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#">Favorites</Nav.Link>
+      <Nav.Link href="#">Wish List</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
