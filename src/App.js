d8dae9176fd1275bd2d8071543001c6d434faacb
Mallory Daniel <mllr.dnl@gmail.com>
2022-02-18 18:45:30 -0800

    fixed babel configs to be able to use FontAwesome

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'; 
 
 function App() {
   return (
@@ -14,16 +16,16 @@ function App() {
         <Container fluid>
           <Row>
             <Navbar className="d-flex" bg="dark" variant="dark">
-            
-            <Navbar.Brand href="#home">BrewBrowser</Navbar.Brand>
-            <Nav className="d-flex justify-content-end">
-              <Nav.Link href="#home">Home</Nav.Link>
-              
-              <Nav.Link href="#">Favorites</Nav.Link>
+            <Col xs={2}>
+            <Navbar.Brand href="#home" className="justify-content-start ms-0">BrewBrowser</Navbar.Brand>
+            </Col>
+            <Col>
+            <Nav className="justify-content-end">
+              <Nav.Link href="#" >Favorites</Nav.Link>
               <Nav.Link href="#">Wish List</Nav.Link>
-              
+              <Nav.Link href="#"><FontAwesomeIcon icon={solid('user')} /></Nav.Link>
             </Nav>
-        
+            </Col>
           </Navbar>
       
       </Row>
